dc1e85c11908261ad4f00297761ae1d192485380
johny1614 <johny1614@gmail.com>
2024-09-16 20:38:33 +0200

    dziala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {greet} from "rx-vis-lib-new";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'rxUser';
+
+  constructor() {
+  }
+
+  makeGreet(){
+    return greet('World xdd');
+  }
 }
