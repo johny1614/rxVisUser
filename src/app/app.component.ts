4c3b75acecd41eb115c45274b99b106fd5fd7b61
Michal Lis <johny1614@gmail.com>
2025-04-20 21:22:35 +0200

    lekkie poprawki nic isttotnego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,44 +17,23 @@ export class AppComponent {
 
   constructor() {
 
-    initRxVision(['stream1', 'stream2',
-      'stream3',
-      'stream4',
-      'stream5',
-      'stream6',
-      'stream7',
+    initRxVision(['stream1', 'stream2'],window);
 
-      'stream8',
-      'stream9',
-      'stream10',
-    ],window);
-    addRxVisionEmission('stream1', this.randomNumber);
-    addRxVisionEmission('stream2', 'value2');
-    addRxVisionEmission('stream3', 'xd');
-    addRxVisionEmission('stream4', 'xd');
-    addRxVisionEmission('stream5', 'xd');
-    addRxVisionEmission('stream6', 'xd');
-    addRxVisionEmission('stream7', 'xd');
-    addRxVisionEmission('stream8', 'xd');
-    addRxVisionEmission('stream9', 'xd');
-    addRxVisionEmission('stream10', 'xd');
+    setTimeout(() => {
+      initRxVision(['stream1', 'stream2'], window);
+    }, 1000);
 
+    setTimeout(() => {
+      addRxVisionEmission('stream1', this.randomNumber);
+    }, 2000)
 
-    // setTimeout(() => {
-    //   initRxVision(['stream1', 'stream2'], window);
-    // }, 1000);
-    //
-    // setTimeout(() => {
-    //   addRxVisionEmission('stream1', this.randomNumber);
-    // }, 2000)
-    //
-    // setTimeout(() => {
-    //   addRxVisionEmission('stream1', 'value2');
-    // }, 3000)
-    //
-    // setTimeout(() => {
-    //   addRxVisionEmission('stream2', 'xd');
-    // }, 4000)
+    setTimeout(() => {
+      addRxVisionEmission('stream1', 'value2');
+    }, 3000)
+
+    setTimeout(() => {
+      addRxVisionEmission('stream2', 'xd');
+    }, 4000)
 
   }
 }
